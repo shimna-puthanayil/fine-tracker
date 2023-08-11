4a9886633d0779e08c774874231cddd1a7e684b8
shimna <chimmu.p@gmail.com>
2023-08-12 01:36:23 +1000

    added &  displayed employees and added functionality to update employee role

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO employee(first_name,last_name,role_id) VALUES('John','Doe',7);
+INSERT INTO employee(first_name,last_name,role_id) VALUES('James','Philip',7);
+INSERT INTO employee(first_name,last_name,role_id) VALUES('Natasha','Francis',7);
