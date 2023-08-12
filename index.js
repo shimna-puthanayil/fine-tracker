094f8ed0497655a9e1c02dc7aa5cb37f3789148d
shimna <chimmu.p@gmail.com>
2023-08-12 22:20:37 +1000

    added title of application using figlet and chalk npm packages

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const CLI = require('./lib/cli.js');
-
-
+//package imported to create ASCII art
+var figlet = require("figlet");
+//package imported for text color 
+const chalk = require("chalk");
 const cli = new CLI();
 
-// function to initiate the application
-cli.run();
+// function to display the title of application
+
+function displayTitle() {
+    console.log(chalk.blackBright("-------------------------------------------------------------------------------------------------------"));
+    console.log(chalk.blackBright("-------------------------------------------------------------------------------------------------------"));
+    console.log("  ");
+    // Creates ASCII Art from text
+    figlet("   EMPLOYEE  TRACKER", function (err, data) {
+
+        if (err) {
+            console.log("Something went wrong...");
+            console.dir(err);
+            return;
+        }
+        console.log(chalk.cyan(data));
+        console.log("");
+        console.log(chalk.blackBright("-------------------------------------------------------------------------------------------------------"));
+        console.log(chalk.blackBright("-------------------------------------------------------------------------------------------------------"));
+        console.log("");
+        // function to initiate the prompt
+        cli.run();
+    });
+}
+displayTitle();
